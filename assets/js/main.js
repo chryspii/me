14fd8d9469779c91438ea7622fd2333fa9e621d5
Chryspii <vicky.chryst@gmail.com>
2025-10-09 11:51:13 +0800

    renew resume

--- assets/js/main.js
+++ assets/js/main.js
@@ -105,7 +105,7 @@
 
   window.addEventListener('load', async () => {
     let count = 0
-    const text = 'Developer'
+    const text = 'Software Engineer'
     const speed = 50
   
     // wait content to load for 1 second
